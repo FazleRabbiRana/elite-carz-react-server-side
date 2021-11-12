655bf73a1260ea8cd987b54ec8a0e638b9f9dc14
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-11-13 01:09:16 +0600

    user update method added

--- index.js
+++ index.js
@@ -44,6 +44,16 @@ async function run() {
 			res.json(result);
 		});
 
+		// update user (role)
+		app.put('/users', async (req, res) => {
+			const user = req.body;
+			const filter = {email: user.email};
+			const options = { upsert: true };
+			const updateDoc = { $set: user };
+			const result = await usersCollection.updateOne( filter, updateDoc, options);
+			res.json(result); 
+		});
+
 		// check an user if admin
 		app.get('/users/:email', async (req, res) => {
 			const email = req.params.email;
