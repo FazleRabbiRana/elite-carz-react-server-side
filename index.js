1c833139b3dbce8896d28915cda2e206fc9d7051
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-11-13 01:38:18 +0600

    user admin check updated

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
 			const query = {email: email};
 			const user = await usersCollection.findOne(query);
 			let isAdmin = false;
-			if (user?.role.toLowerCase() === 'admin') {
+			if (user?.role === 'admin') {
 				isAdmin = true;
 			}
 			res.json({ admin: isAdmin });
