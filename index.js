2c4c257095fb262dd747fb4a2f9bc48a480b3bec
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-11-14 02:44:30 +0600

    product delete method added

--- index.js
+++ index.js
@@ -90,6 +90,14 @@ async function run() {
 			res.json(result);
 		});
 
+		// delete a product
+		app.delete('/products/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = {_id: ObjectId(id)};
+			const result = await productsCollection.deleteOne(query);
+			res.json(result);
+		});
+
 		// add an user
 		app.post('/users', async (req, res) => {
 			const user = req.body;
